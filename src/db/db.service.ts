f9eb132d8c91c092c6d2519b4cd69b387cbabbd1
JEdaurdoPA <A01666860@tec.mx>
2025-09-22 19:21:49 -0600

    primer commit

---
+++ src/db/db.service.ts
@@ -0,0 +1,26 @@
+/* eslint-disable prettier/prettier */
+
+import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
+import { Pool, createPool } from "mysql2/promise";
+
+@Injectable()
+export class DbService implements OnModuleInit, OnModuleDestroy{
+    private pool: Pool;
+
+    onModuleInit():void {
+        this.pool = createPool({
+            host: 'localhost',
+            user: 'root',
+            password: 'andrestorres',
+            database: '451_demo',
+        })
+    }
+    onModuleDestroy() {
+       void this.pool.end();
+    }
+
+    getPool():Pool{
+        return this.pool;
+    }
+    
+}
